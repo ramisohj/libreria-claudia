0f4d227675689551143866c1856f4fc80948ab7d
ramisohj <ramisohj@gmail.com>
2019-05-12 06:25:18 -0700

    adding firebase for environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyCiXeQ-HzbtkesKvl0d_FMe3ix_r8t1DNs",
+    authDomain: "libreria-claudia-5a635.firebaseapp.com",
+    databaseURL: "https://libreria-claudia-5a635.firebaseio.com",
+    projectId: "libreria-claudia-5a635",
+    storageBucket: "libreria-claudia-5a635.appspot.com",
+    messagingSenderId: "160122003341",
+    appId: "1:160122003341:web:1098cc71853cc161"
+  }
 };
