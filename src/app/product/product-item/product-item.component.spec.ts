41f8ddbf1a32d04caebe38e845cecf3abfb9e5cb
KennyV91 <velasco_ariel_123@hotmail.com>
2019-05-25 00:09:17 -0400

    Test borrar boton

--- src/app/product/product-item/product-item.component.spec.ts
+++ src/app/product/product-item/product-item.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ProductItemComponent } from './product-item.component';
+import { from } from 'rxjs';
 
 describe('ProductItemComponent', () => {
   let component: ProductItemComponent;
   let fixture: ComponentFixture<ProductItemComponent>;
+  let instance: ProductItemComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,4 +25,10 @@ describe('ProductItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('sould erase the prduct', () =>{
+    spyOn(instance, 'deleteItem');
+    const button = fixture.debugElement.query(By.css('button'));
+    button.nativeElement.click();
+    expect(instance.deleteItem).toHaveBeenCalled();
+  })
 });
