ba0275b0741a737316987f49e2a4dd12d38829d7
cr0wg4n <mcm.crw@gmail.com>
2019-06-11 01:01:16 -0400

    bug de eliminacion con refrescamiento resuelto

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/ProductModel';
 import { CartService } from '../services/cart.service';
 import { CartModel } from '../models/CartModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -18,16 +19,19 @@ export class CartComponent implements OnInit {
     @Input()
     products:Product[];
 
-    constructor(private cartService : CartService) { 
+    constructor(private cartService : CartService, private router: Router) { 
         this.cartModel$ = cartService.cartModel;
         this.items$ = cartService.cartModel.cartShopping$;
     }
 
     ngOnInit() {
-
       this.cartService.cast.subscribe(cartModel => {
         this.cartModel$ = cartModel;
         this.items$ = this.cartService.cartModel.cartShopping$;
-      })      
+        console.log( this.items$,'hola');   
+      });
+    }
+    clear(){
+      localStorage.clear();
     }
 }
